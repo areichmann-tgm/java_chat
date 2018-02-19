d0eaf5145a8ce3cc51ad50cd3c7242191a532e8f
areichmann-tgm <areichmann@student.tgm.ac.at>
2018-02-19 09:05:38 +0100

    first commit

--- src/simple_server.java
+++ src/simple_server.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
-public class simple_server {
+public class simple_server extends Thread{
+	
+	public simple_server() {
+		try (
+			    Socket echoSocket = new Socket(hostName, portNumber);
+			    PrintWriter out =
+			        new PrintWriter(echoSocket.getOutputStream(), true);
+			    BufferedReader in =
+			        new BufferedReader(
+			            new InputStreamReader(echoSocket.getInputStream()));
+			    BufferedReader stdIn =
+			        new BufferedReader(
+			            new InputStreamReader(System.in))
+			)
+	}
+	
+	public void run() {
+        
+    }
 
 }
